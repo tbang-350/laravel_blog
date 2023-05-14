d192d786d408a398404e66a4752a64535645f5ef
tbang-350 <tbway350@gmail.com>
2023-05-14 10:33:57 +0300

    added user profile view page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,14 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 
 class AdminController extends Controller
 {
 
-    /**
-     * Destroy an authenticated session.
-     */
     public function destroy(Request $request)
     {
         Auth::guard('web')->logout();
@@ -21,6 +19,17 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/login');
+    } // End Method
+
+
+    public function Profile(){
+
+        $id = Auth::user()->id;
+
+        $adminData = User::find($id);
+
+        return view('admin.admin_profile_view',compact('adminData'));
+
     } // End Method
 
 }
